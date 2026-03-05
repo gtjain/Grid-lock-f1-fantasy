837e0e9c57d5f0fe6acbf469f2873a992b44d21d
gtjain <gtjain12345@gmail.com>
2026-03-05 13:25:52 +0530

    Remove invalid dynamic export from server action file

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,7 +1,5 @@
 "use server";
 
-export const dynamic = "force-dynamic";
-
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth/next";
